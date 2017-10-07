313b3f3dccf97133dd5ba0d0008dca91b38c2d29
woraprat <woraprat.bo.56@ubu.ac.th>
2017-10-07 15:51:29 +0700

    fix route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import{ RouterModule, Routes } from  '@angular/router';
 
 const Routes: Routes =[
     { path:'contact',component: ContactComponent },
-    { path: 'home',component:HomeComponent }
+    { path: '**',component:HomeComponent }
 
 ];
 
