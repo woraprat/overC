7f5a1d8aedb4c396e668db64d95755033d9b2277
woraprat <woraprat.bo.56@ubu.ac.th>
2018-02-12 22:49:19 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,19 @@ import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServiceComponent } from './service/service.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    HomeComponent
-   
-  ],
+    HomeComponent,
+    AboutUsComponent,
+    ServiceComponent,
+    ProductComponent
+],
   imports: [
     BrowserModule,
     AppRouting
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,15 @@
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServiceComponent } from './service/service.component';
+import { ProductComponent } from './product/product.component';
 import{ RouterModule, Routes } from  '@angular/router';
 
 const Routes: Routes =[
     { path:'contact',component: ContactComponent },
+    { path:'about-us',component: AboutUsComponent},
+    { path:'service',component: ServiceComponent},
+    { path:'product',component: ProductComponent},
     { path: '**',component:HomeComponent }
 
 ];
